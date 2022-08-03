4a0c59b098b1c21397d634a5dba9ca07a35a5315
Alexander Lazaris <alex.lazaris617@gmail.com>
2022-08-03 16:57:14 +1000

    increased test coverage

--- tests/multiply.test.js
+++ tests/multiply.test.js
@@ -1,7 +1,26 @@
 const multiply = require('../src/multiply');
 
 describe('test multiply positive scenarios', () => {
+    beforeAll(() => {
+        const date = new Date();
+        const startTime = `${date.toTimeString()} - ${date.getMilliseconds()}ms`
+        console.log(`before > ${startTime}`);
+    });
     test('multiply 3 by 2 should equal to 6', () => {
         expect(multiply(3, 2)).toBe(6);
     })
+
+    test('multiply -7 by 9 should equal to 63', () => {
+        expect(multiply(-7, 9)).not.toBe(63);
+    })
+
+    test('multiply 1 by 1 should equal to 1 and be true', () => {
+        expect(multiply(1, 1)).toBeTruthy();
+    })
+
+    afterAll(() => {
+        const date = new Date();
+        const endTime = `${date.toTimeString()} - ${date.getMilliseconds()}ms`
+        console.log(`after > ${endTime}`);
+    });
 })
