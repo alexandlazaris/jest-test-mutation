308f178ac11f65459981f9f376c4cd348c3cb2fe
Alexander Lazaris <alex.lazaris617@gmail.com>
2022-08-04 15:56:47 +1000

    added another test

--- tests/multiply.test.js
+++ tests/multiply.test.js
@@ -7,16 +7,20 @@ describe('test multiply positive scenarios', () => {
         console.log(`before > ${startTime}`);
     });
     test('multiply 3 by 2 should equal to 6', () => {
-        expect(multiply(3, 2)).toBe(6);
+        expect(multiply.multiply(3, 2)).toBe(6);
     })
 
     test('multiply -7 by 9 should equal to 63', () => {
-        expect(multiply(-7, 9)).not.toBe(63);
+        expect(multiply.multiply(-7, 9)).not.toBe(63);
     })
 
     test('multiply 1 by 1 should equal to 1 and be true', () => {
-        expect(multiply(1, 1)).toBeTruthy();
+        expect(multiply.multiply(1, 1)).toBeTruthy();
     })
+
+    test('multiple 2 by 10 using negative function should -20', () => {
+        expect(multiply.multiplyForceNegativeResult(2, 10)).toBe(-20);
+    });
 
     afterAll(() => {
         const date = new Date();
